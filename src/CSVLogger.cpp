5383430792685ab943bdc9ac0757c9506582aeca
ayberkozgur <ayberk.ozgur@epfl.ch>
2016-11-14 11:19:52 +0100

    Added CSV logger proto

---
+++ src/CSVLogger.cpp
@@ -0,0 +1,179 @@
+/*
+ * Copyright (C) 2016 EPFL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+/**
+ * @file CSVLogger.cpp
+ * @brief Source for a QML CSV file logger
+ * @author Ayberk Özgür
+ * @date 2016-06-15
+ */
+
+#include "CSVLogger.h"
+
+#include <QDir>
+#include <QStandardPaths>
+#include <QDateTime>
+#include <QSysInfo>
+#include <QNetworkInterface>
+#include <QBluetoothLocalDevice>
+
+CSVLogger::CSVLogger(QQuickItem* parent) : QQuickItem(parent){
+    logTime = true;
+    logMillis = true;
+
+    fileNeedsReopen = false;
+
+    toConsole = false;
+
+    /*
+     * Build device ID string
+     */
+
+    deviceId = QSysInfo::prettyProductName();
+
+    QString macAddr("");
+
+    //Try Bluetooth first, it is more reliable than WiFi and Ethernet
+    for(QBluetoothHostInfo& info : QBluetoothLocalDevice::allDevices()){
+        QString hwAddr(info.address().toString());
+        if(hwAddr != "00:00:00:00:00:00" && hwAddr != "02:00:00:00:00:00"){
+            macAddr = hwAddr;
+            break;
+        }
+    }
+
+    //Try WiFi/Ethernet next
+    if(macAddr == "")
+        for(QNetworkInterface& interface : QNetworkInterface::allInterfaces())
+            if(!(interface.flags() & QNetworkInterface::IsLoopBack)){
+                QString hwAddr = interface.hardwareAddress();
+                qDebug() << hwAddr;
+                if(hwAddr != "00:00:00:00:00:00"){
+                    macAddr = hwAddr;
+                    break;
+                }
+            }
+
+    #ifdef ANDROID
+
+        //Try /sys/class/net/wlan0/address for Android 6
+        if(macAddr == ""){
+            QFile wifiMacAddressFile("/sys/class/net/wlan0/address");
+            if(wifiMacAddressFile.open(QIODevice::ReadOnly | QIODevice::Text)){
+                QString hwAddr = wifiMacAddressFile.readLine();
+                hwAddr = hwAddr.replace("\n","");
+                wifiMacAddressFile.close();
+                qDebug() << hwAddr;
+                if(hwAddr != "00:00:00:00:00:00" && hwAddr != "02:00:00:00:00:00")
+                    macAddr = hwAddr;
+            }
+        }
+    #endif
+
+    if(macAddr != "")
+        deviceId += " @ " + macAddr;
+    else
+        qWarning() << "Logger: Couldn't get any MAC address of device, device ID won't be unique!";
+}
+
+CSVLogger::~CSVLogger(){
+    writer.flush();
+    file.close();
+}
+
+inline QString CSVLogger::linePrefix(){
+    QString res = "";
+    if(logTime){
+        if(logMillis)
+            res += QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss.zzz");
+        else
+            res += QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss");
+    }
+    return res;
+}
+
+void CSVLogger::setLogTime(bool logTime){
+    if(this->logTime != logTime){
+        this->logTime = logTime;
+        emit logTimeChanged();
+    }
+}
+
+void CSVLogger::setLogMillis(bool logMillis){
+    if(this->logMillis != logMillis){
+        this->logMillis = logMillis;
+        emit logMillisChanged();
+    }
+}
+
+void CSVLogger::setFilename(const QString& filename){
+    if(this->filename != filename){
+        writer.flush();
+        file.close();
+
+        this->filename = filename;
+
+        fileNeedsReopen = true;
+
+        emit filenameChanged();
+    }
+}
+
+void CSVLogger::log(const QString& data){
+    if(!isEnabled())
+        return;
+
+    if(toConsole){
+        qDebug() << linePrefix() + data;
+        return;
+    }
+
+    //File needs re-opening
+    if(fileNeedsReopen){
+        QDir dir(filename);
+        if(dir.isAbsolute())
+            qDebug() << "Logger: Opening " + filename + " to log.";
+        else{
+            filename = QStandardPaths::writableLocation(QStandardPaths::StandardLocation::DocumentsLocation) + "/" + filename;
+            qDebug() << "Logger: Absolute path not given, opening " + filename + " to log.";
+            emit filenameChanged();
+        }
+        QDir::root().mkpath(QFileInfo(filename).absolutePath());
+
+        file.setFileName(filename);
+        if(!file.open(QIODevice::WriteOnly | QIODevice::Append)){
+            qCritical() << "Logger: Could not open file.";
+            return;
+        }
+        else
+            writer.setDevice(&file);
+
+
+        //// DUMP HEADER
+
+
+        fileNeedsReopen = false;
+    }
+
+    //Actual data logging
+    if(file.isOpen()){
+        writer << linePrefix() << data << "\n";
+        writer.flush();
+    }
+    else
+        qCritical() << "Logger: Attempted log() but file is not open, valid filename must be provided beforehand.";
+}
---
+++ src/CSVLogger.h
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2016 EPFL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+/**
+ * @file CSVLogger.h
+ * @brief Header for a QML CSV file logger
+ * @author Ayberk Özgür
+ * @date 2016-06-15
+ */
+
+#ifndef CSVLOGGER_H
+#define CSVLOGGER_H
+
+#include <QQuickItem>
+#include <QString>
+#include <QFile>
+
+class CSVLogger : public QQuickItem {
+    /* *INDENT-OFF* */
+    Q_OBJECT
+    /* *INDENT-ON* */
+
+    Q_PROPERTY(bool logTime WRITE setLogTime READ getLogTime NOTIFY logTimeChanged)
+    Q_PROPERTY(bool logMillis WRITE setLogMillis READ getLogMillis NOTIFY logMillisChanged)
+    Q_PROPERTY(bool deviceId READ getDeviceId CONSTANT)
+    Q_PROPERTY(QString filename WRITE setFilename READ getFilename NOTIFY filenameChanged)
+    Q_PROPERTY(bool toConsole MEMBER toConsole)
+
+
+
+    Q_PROPERTY(qstringarr header WRITE READ NOTIFY) //setter should give qwarning() << "not editable" after first log
+
+public:
+
+    /**
+     * @brief Creates a new CSVLogger with the given QML parent
+     *
+     * @param parent The QML parent
+     */
+    CSVLogger(QQuickItem* parent = 0);
+
+    /**
+     * @brief Destroys this CSVLogger
+     */
+    ~CSVLogger();
+
+    /**
+     * @brief Sets the file name; puts file to home directory if full path is not given
+     *
+     * @param filename The new filename, or full path
+     */
+    void setFilename(const QString& filename);
+
+    /**
+     * @brief Gets the filename
+     *
+     * @return The filename
+     */
+    QString getFilename(){ return filename; }
+
+    /**
+     * @brief Sets whether to timestamp the log lines
+     *
+     * @param logTime Whether to include timestamp
+     */
+    void setLogTime(bool logTime);
+
+    /**
+     * @brief Gets whether to timestamp the log lines
+     *
+     * @return Whether to timestamp the log lines
+     */
+    bool getLogTime(){ return logTime; }
+
+    /**
+     * @brief Sets whether to include milliseconds in timestamp
+     *
+     * @param logMillis Whether to include milliseconds in timestamp
+     */
+    void setLogMillis(bool logMillis);
+
+    /**
+     * @brief Gets whether to include milliseconds in timestamp
+     *
+     * @return Whether to include milliseconds in timestamp
+     */
+    bool getLogMillis(){ return logMillis; }
+
+    /**
+     * @brief Gets the unique device ID
+     *
+     * @return The unique device ID
+     */
+    QString getDeviceId(){ return deviceId; }
+
+signals:
+
+    /**
+     * @brief Emitted when logTime changes
+     */
+    void logTimeChanged();
+
+    /**
+     * @brief Emitted when logMillis changes
+     */
+    void logMillisChanged();
+
+    /**
+     * @brief Emitted when filename changes
+     */
+    void filenameChanged();
+
+public slots:
+
+    /**
+     * @brief Logs given data as one line to file
+     *
+     * @param data Data to log
+     */
+    void log(qstringable array log);//const QString& data);
+
+private:
+
+    bool logTime;           ///< Whether to include time when data is logged
+    bool logMillis;         ///< Whether to include milliseconds when logging time
+    QString filename;       ///< Log's filename or full path
+    bool fileNeedsReopen;   ///< Filename changed and file needs reopening
+
+    QFile file;             ///< Log file
+    QTextStream writer;     ///< Log file writer
+
+    bool toConsole;         ///< Log to console instead of file for debug purposes
+
+    QString deviceId;       ///< Unique device ID
+
+    /**
+     * @brief Builds the log line info prefix
+     *
+     * @return Log line info prefix
+     */
+    QString linePrefix();
+
+};
+
+#endif /* CSVLOGGER_H */
